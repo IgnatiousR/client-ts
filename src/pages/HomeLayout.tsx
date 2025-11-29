cb5fb3edb12bdd5a67b31fa1b4bbb684aff42a4c
Ignatious <ignatiousr80@gmail.com>
2025-11-29 16:54:12 +0600

    updated

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -3,7 +3,8 @@ const HomeLayout = () => {
     <div>
       <h1>Home Layout</h1>
       <p>Home Layout</p>
-      <h1 className="text-fluid-h1">Title TEst</h1>
+      <button className="btn">Button</button>
+      <button className="btn">Button 2</button>
     </div>
   );
 };
