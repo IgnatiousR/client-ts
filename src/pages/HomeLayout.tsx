b64fa96e266dcf5c310594f091155b5eb72d0f5b
Ignatious <ignatiousr80@gmail.com>
2025-12-01 09:30:54 +0600

    update

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,11 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const HomeLayout = () => {
   return (
-    <div>
-      <h1>Home Layout</h1>
-      <p>Home Layout</p>
-      <button className="btn">Button</button>
-      <button className="btn">Button 2</button>
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 };
 
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,36 @@
+// import styled from "styled-components";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <section>
+      <nav className="flex h-24 mx-auto items-center max-w-[90vw]">
+        <img src={logo} alt="jobsite" className="logo" />
+      </nav>
+      <div className="cont page">
+        <div className="info">
+          <h1 className="font-bold mb-6">
+            job <span className="text-primary-500">tracking</span> app
+          </h1>
+          <p className="leading-loose mb-6 max-w-[35em]">
+            I'm baby man bun +1 fit palo santo austin lumbersexual sus. Fingerstache helvetica
+            polaroid cold-pressed hashtag, 3 wolf moon vape waistcoat man bun four loko direct trade
+            tousled taxidermy leggings. Fit edison bulb tumeric blackbird spyplane, trust fund
+            ethical thundercats banjo praxis same twee.
+          </p>
+          <Link to="/register" className="btn mr-4">
+            Register
+          </Link>
+          <Link to="/register" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img hidden lg:block" />
+      </div>
+    </section>
+  );
 };
 
 export default Landing;
